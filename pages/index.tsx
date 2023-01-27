b73f40ba9bb3e53638a08c130de40c3d351c21d2
dv <dvaug86@gmail.com>
2023-01-27 12:39:32 -0500

    work experience page added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,33 +4,36 @@ import { NextPage } from 'next'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
+import WorkExperience from '@/components/WorkExperience'
 
 
- const Home: NextPage = () => {
+const Home: NextPage = () => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Sonny's Portfolio</title>
       </Head>
 
-<Header />
-{/* Hero */}
-<section id='hero' className='snap-start'>
-<Hero />
-</section>
+      <Header />
+      <section id='hero' className='snap-start'>
+        <Hero />
+      </section>
 
-{/* About */}
-<section id='about' className='snap-center'>
-  <About />
-</section>
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
 
-{/* Experience */}
+      {/* Experience */}
+      <section id='experience' className='snap-center'>
+        <WorkExperience />
+      </section>
 
-{/* Skills */}
 
-{/* Projects */}
+      {/* Skills */}
 
-{/* Contact Me */}
+      {/* Projects */}
+
+      {/* Contact Me */}
 
 
 
