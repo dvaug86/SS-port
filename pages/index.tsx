7407327cc685328836a72c149fc867ee742606f5
dv <dvaug86@gmail.com>
2023-01-27 14:43:11 -0500

    skill section added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import WorkExperience from '@/components/WorkExperience'
+import Skills from '@/components/Skills'
 
 
 const Home: NextPage = () => {
@@ -23,13 +24,14 @@ const Home: NextPage = () => {
         <About />
       </section>
 
-      {/* Experience */}
       <section id='experience' className='snap-center'>
         <WorkExperience />
       </section>
 
-
       {/* Skills */}
+      <section id='experience' className='snap-start'>
+        <Skills />
+      </section>
 
       {/* Projects */}
 
